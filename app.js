90c779dcfc20cb260cd5898e6857b55a4724e657
JASantia85 <JuanASantiago1058@gmail.com>
2022-09-08 19:19:46 -0400

    create some actions

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 const initialState = [ 'Take Five', 'Claire de Lune', 'Respect' ];
+
+const addNewSong = {
+  type: 'songs/addSong',
+  payload: 'Halo'
+};
+
+const removeSong = {
+  type: 'songs/removeSong',
+  payload: 'Take Five'
+};
+
+const removeAll = {
+  type: 'songs/removeAll'
+}
