199f1f9201922a4238b8bb7a9586ca2e6375f2df
JASantia85 <JuanASantiago1058@gmail.com>
2022-09-08 19:49:13 -0400

    fix removeItemAtIndex

--- app.js
+++ app.js
@@ -32,3 +32,15 @@ const removeAll = {
 // 1. They should only calculate the new state value based on the state and action arguments.
 // 2. They are not allowed to modify the existing state. Instead, they must copy the existing state and make changes to the copied values.
 // 3. They must not do any asynchronous logic or have other “side effects”.
+
+const removeItemAtIndex = (list, index) => {
+  // since it is wraped around [], it will return an array, which is the original data structure for state
+  return [
+    // first slice removes the element that we want
+    ...list.slice(0, index),
+    // second slice copies from that remove element to the end
+    ...list.slice(index + 1, list.length),
+  ];
+};
+
+console.log(removeItemAtIndex(['a', 'b', 'c', 'd'], 1));
